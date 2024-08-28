996b187aec05b6ce00f376c0b4bd2e33ee1f8a29
Joyce <151400683+joyce-ship-it@users.noreply.github.com>
2024-08-28 19:49:08 +0530

    Complete Image slider

--- src/about.js
+++
@@ -1,9 +0,0 @@
-function about(element){
-    const article =  document.createElement('article');
-    article.textContent = `Chocolate factory was founded in 2024 by a job seeker named Zai Whitaker
-    due to his love for dark, unadulterated chocolate. The company intends on producing high quality affordable 
-    chocolate to the masses at affordable prices.`;
-    element.append(article);
-}
-
-export {about};
--- src/index.js
+++ src/index.js
@@ -1,43 +1,72 @@
-console.log('Restaurant');
 import "./styles.css";
-import {home} from "./home.js";
-import {menu} from "./menu.js";
-import {about} from "./about.js";
 
-const content = document.querySelector('#content');
-const buttonArr = document.querySelectorAll('button');
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const images = document.querySelectorAll(".slider > img");
+const imgArr = Array.from(images);
+const size = imgArr.length;
+const optionsDiv = document.querySelector(".options");
+const optionsArr = Array.from(document.querySelectorAll(".options > button"));
 
-buttonArr[0].classList.add('selected');
-home(content);
+imgArr.forEach((img, index) => {
+  img.dataset.index = index;
+});
 
-function removeSelected(buttonArr){
-    buttonArr.forEach(button => {
-        if(button.classList.contains('selected')){
-            button.classList.remove('selected');
-        }
-    });
-}
+const makeAllImgHidden = function (imgArr) {
+  imgArr.forEach((img) => {
+    img.classList.add("hidden");
+  });
+};
 
-buttonArr[0].addEventListener('click',(e)=>{
-    removeSelected(buttonArr);
-    e.target.classList.add('selected');
+const toggleUnselectedOptions = function (optionsArr) {
+  const [currOption, ...discard] = optionsArr.filter((option) =>
+    option.classList.contains("selected"),
+  );
+  const currOptionIndex = Number(currOption.dataset.index);
+  optionsArr[currOptionIndex].classList.toggle("selected");
+};
 
-    content.textContent = "";
-    home(content);
-})
+makeAllImgHidden(imgArr);
 
-buttonArr[1].addEventListener('click', (e)=>{
-    removeSelected(buttonArr);
-    e.target.classList.add('selected');
+imgArr[0].classList.toggle("hidden");
+optionsArr[0].classList.toggle("selected");
 
-    content.textContent = "";
-    menu(content);
-})
+const moveLeft = function () {
+  const [currImg, ...discard] = imgArr.filter(
+    (img) => !img.classList.contains("hidden"),
+  );
+  const currImgIndex = Number(currImg.dataset.index);
+  const nextImgIndex = currImgIndex === 0 ? size - 1 : currImgIndex - 1;
+  makeAllImgHidden(imgArr);
+  imgArr[nextImgIndex].classList.toggle("hidden");
+  toggleUnselectedOptions(optionsArr);
+  optionsArr[nextImgIndex].classList.toggle("selected");
+};
 
-buttonArr[2].addEventListener('click', (e)=>{
-    removeSelected(buttonArr);
-    e.target.classList.add('selected');
-    
-    content.textContent = "";
-    about(content);
-})
+leftArrow.addEventListener("click", moveLeft);
+
+const moveRight = function () {
+  const [currImg, ...discard] = imgArr.filter(
+    (img) => !img.classList.contains("hidden"),
+  );
+  const currImgIndex = Number(currImg.dataset.index);
+  const nextImgIndex = currImgIndex === size - 1 ? 0 : currImgIndex + 1;
+  makeAllImgHidden(imgArr);
+  imgArr[nextImgIndex].classList.toggle("hidden");
+  toggleUnselectedOptions(optionsArr);
+  optionsArr[nextImgIndex].classList.toggle("selected");
+};
+
+rightArrow.addEventListener("click", moveRight);
+
+optionsDiv.addEventListener("click", (e) => {
+  // You will get an error if you click on the div instead of the button
+  // fix : check it the event comes from a button or not
+  if (e.target.matches("button")) {
+    const selectedIndex = Number(e.target.dataset.index);
+    makeAllImgHidden(imgArr);
+    imgArr[selectedIndex].classList.toggle("hidden");
+    toggleUnselectedOptions(optionsArr);
+    optionsArr[selectedIndex].classList.toggle("selected");
+  }
+});
